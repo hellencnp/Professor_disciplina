b5ce65cfd3094aad50c050af302929e24e47d51d
hellencnp <69966800+hellencnp@users.noreply.github.com>
2021-12-01 01:44:37 -0300

    Update MenuBotoes.js

--- frontend/src/componentes/MenuBotoes/MenuBotoes.js
+++ frontend/src/componentes/MenuBotoes/MenuBotoes.js
@@ -4,9 +4,8 @@ export default function MenuBotoes() {
   return (
     <div className = "menuBotoes">
 
-      <button type="button" id="btnProfessores" className="btn btn-secondary">Professores</button>
-      <button type="button" id="btnDisciplinas" className="btn btn-success">Disciplinas</button>
+      <a type="button" id="btnProfessores" className="btn btn-secondary"href="/professores">Professores</a>
+      <a type="button" id="btnDisciplinas" className="btn btn-success" href="/disciplinas">Disciplinas</a>
     </div>
   );
 }
-
